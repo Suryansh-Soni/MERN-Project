0097a43fafb1cc55a5ab57b1dfc41c18ee730d74
Suryansh Soni <suryanshsoni92@gmail.com>
2026-05-26 14:57:11 +0530

    listing schema

--- models/listing.js
+++ models/listing.js
@@ -1,30 +1,48 @@
 const mongoose = require("mongoose");
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type:String,
-        required:true},
-    description:{
-        type:String,
-        required:true},
-    imageUrl:{
-        type:String,
-        required:true,
-        set:(v)=>v==""? "https://www.google.com/imgres?q=image%20of%20snorlax&imgurl=https%3A%2F%2Farchives.bulbagarden.net%2Fmedia%2Fupload%2Fthumb%2F5%2F56%2FSleep_Key_Art.jpg%2F640px-Sleep_Key_Art.jpg&imgrefurl=https%3A%2F%2Fbulbapedia.bulbagarden.net%2Fwiki%2FSnorlax_(Sleep)&docid=B3QvbeZhhr5JxM&tbnid=KjoO_EEYLOlINM&vet=12ahUKEwjZ8rDMo9aUAxUpzDgGHfMOFlQQnPAOegQIGRAB..i&w=640&h=640&hcb=2&ved=2ahUKEwjZ8rDMo9aUAxUpzDgGHfMOFlQQnPAOegQIGRAB" :v
-    },
-    price:{
-        type:Number,
-        required:true}, 
-    location:{
-        type:String,
-        required:true
-    },
-    country:{
-        type:String,
-        required:true
-    }});
+  title: {
+    type: String,
+    required: true,
+  },
 
-const Listing = mongoose.model("Listing",listingSchema);
+  description: {
+    type: String,
+    required: true,
+  },
 
-module.exports=Listing;
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+          : v,
+    },
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  location: {
+    type: String,
+    required: true,
+  },
+
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
