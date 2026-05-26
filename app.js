4ee125d56356df57bea01f6980ed0d9151c64dc6
Suryansh Soni <suryanshsoni92@gmail.com>
2026-05-26 12:11:08 +0530

    app file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const mongoose =require("mongoose");
+const Listing = require("./models/listing");
 
 async function main(){
     await mongoose.connect("mongodb://localhost:27017/wanderlust");
@@ -15,6 +16,20 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.get("/testlistings",async (req,res)=>{
+    let samplelisting=new Listing({
+        title:"Sample Listing",
+        description:"This is a sample listing for testing purposes.",
+        imageUrl:"",
+        price:100,
+        location:"Sample Location",
+        country:"Sample Country"
+    })
+    await samplelisting.save();
+    console.log("Sample listing created and saved to the database.");
+    res.send("Sample listing created and saved to the database.");
+})
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
 })
